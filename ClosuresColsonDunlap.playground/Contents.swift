bf1887c52d741f9769d902f2b3b8e9536742f439
Colson Dunlap <parkerdunlap2@gmail.com>
2020-11-19 11:39:11 -0500

    Initial Commit

---
+++ ClosuresColsonDunlap.playground/Contents.swift
@@ -0,0 +1,34 @@
+//Enum for Student type
+enum StudentType{
+    case Mobile
+    case Web
+    case Analytics
+    case Service
+}
+
+//Struct to represent a student
+struct Student{
+    var name: String
+    var type: StudentType
+}
+
+//create array and populate it with 10 Students
+let students = [
+    Student.init(name: "Colson Dunlap", type: .Mobile),
+    Student.init(name: "John Smith", type: .Web),
+    Student.init(name: "Jane Doe", type: .Analytics),
+    Student.init(name: "Sue Jennings", type: .Service),
+    Student.init(name: "Bob Johnson", type: .Mobile),
+    Student.init(name: "Randy Mabry", type: .Web),
+    Student.init(name: "Ashley Rummage", type: .Analytics),
+    Student.init(name: "Jack Ryan", type: .Service),
+    Student.init(name: "Kate Black", type: .Mobile),
+    Student.init(name: "Will Butler", type: .Web)
+]
+
+//Create closure used to filter based on student type criteria
+let criteria = StudentType.Mobile
+let filteredStudents = students.filter{ $0.type == criteria }
+filteredStudents
+
+
---
+++ MyFirstPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+let course = "CSC 120"
+
+switch(course){
+case "CSC 120":
+    print("CS 1")
+case "CSC 151":
+    print("CS 2")
+default:
+    print("No course")
+}
